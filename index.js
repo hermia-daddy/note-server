67bb98fe7f317c999f91391361aaf531ec08c911
zhongfc <zhongfengcheng@xyac.com.cn>
2020-10-16 17:06:09 +0800

    deploy on internet

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
 
 
 let notes = [
@@ -54,64 +56,91 @@ let notes = [
 ]
 
 
+
+
 const app = express()
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message)
 
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 app.get('/api/notes', (req, res) => {
-    
-    res.json(notes)
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
+
 })
 
-app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        res.json(note)
-    } else {
-        res.status(404).end()
-    }
+app.get('/api/notes/:id', (req, res, next) => {
+    Note.findById(req.params.id)
+        .then(note => {
+            if (note) {
+                res.json(note)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => {
+            next(error)
+        })
 })
-
-generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-    return maxId+1
-}
 
 app.post('/api/notes', (req, res) => {
 
     const body = req.body
-    if(!body.content){
+    if (!body.content) {
         return res.status(400).json({
-            error:'content missing'
+            error: 'content missing'
         })
     }
 
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+    })
+
+    note.save().then(savedNote => {
+        res.json(savedNote)
+    })
+})
+
+app.put('/api/notes/:id', (req, res, next) => {
+    const body = req.body
+
     const note = {
-        content : body.content,
-        important: body.important||false,
-        date:new Date(),
-        id:generateId()
+        content: body.content,
+        important: body.important
     }
 
-    notes = notes.concat(note)
-    console.log(note)
-    res.json(note)
+    Note.findByIdAndUpdate(req.params.id, note, { new: true })
+        .then(updatedNote => {
+            res.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    console.log('delete id:', id)
-    notes = notes.filter(note => note.id !== id)
-
-    res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server runing on port ${PORT}`)
 })
