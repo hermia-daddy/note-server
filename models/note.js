8f4aaa79e5b51a1c37f5fb832af9ecadef159868
zhongfc <zhongfengcheng@xyac.com.cn>
2020-10-17 14:52:32 +0800

    change app structure

--- models/note.js
+++ models/note.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose')
-
-const url = process.env.MONGODB_URI
-console.log('connecting to ', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message);
-    })
-
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
+    content: {
+        type: String,
+        minlength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
     important: Boolean
 })
 
@@ -25,4 +20,4 @@ noteSchema.set('toJSON', {
     }
 })
 
-module.exports = mongoose.model('Note',noteSchema)
+module.exports = mongoose.model('Note', noteSchema)
