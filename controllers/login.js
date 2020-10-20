10160e0f9e05d1ca05a90915b6ffea8a7681cfda
zhongfc <zhongfengcheng@xyac.com.cn>
2020-10-20 13:14:53 +0800

    jwt authorization

---
+++ controllers/login.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/user");
+
+loginRouter.post("/", async (request, response) => {
+  const body = request.body;
+
+  const user = await User.findOne({ username: body.username });
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: "invalid usernam or password",
+    });
+  }
+
+  const userForToken = {
+    usernam: user.username,
+    id: user._id,
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  response.status(200).send({ token, usernam: user.usernam, name: user.name });
+});
+
+module.exports = loginRouter;
