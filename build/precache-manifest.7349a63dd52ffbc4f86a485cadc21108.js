b03c012644bcef794b43f76ca2690ca52fb85208
zhongfc <zhongfengcheng@xyac.com.cn>
2020-10-24 21:14:40 +0800

    end to end test

---
+++ build/precache-manifest.7349a63dd52ffbc4f86a485cadc21108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5332bf1be5b7e5e9a2653bfcd34c1d22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec50fbdf48b615cacc78",
+    "url": "/static/css/main.fa8aaaf5.chunk.css"
+  },
+  {
+    "revision": "03b5d75e97b15725f5dc",
+    "url": "/static/js/2.d3ad4f08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3ad4f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec50fbdf48b615cacc78",
+    "url": "/static/js/main.1c42af61.chunk.js"
+  },
+  {
+    "revision": "2cadfdd0f51b6099da8d",
+    "url": "/static/js/runtime-main.64a09c29.js"
+  }
+]);
